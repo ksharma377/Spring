2960c4bea3d31ed047ddfe7ec0dd69cf089f95c2
Kshitij Sharma <ksharma377@gmail.com>
2020-04-07 11:52:40 +0530

    Updated pom

--- Spring Boot/calculator/src/test/java/com/service/calculator/CalculatorServiceApplicationTests.java
+++ Spring Boot/calculator/src/test/java/com/service/calculator/CalculatorServiceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.service.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatorServiceApplicationTests {
 
+	@Mock
+	CalculatorServiceApplication calculatorServiceApplicationInstance;
+
 	@Test
-	void contextLoads() {
+	void add(double x, double y) {
+
 	}
 
 }
